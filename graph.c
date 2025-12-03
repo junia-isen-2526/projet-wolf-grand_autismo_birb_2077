67dcc0c98d91a7199268dfe9a8190414ab6a13f2
kheldae <taho.gougeon@gmail.com>
2025-12-03 17:28:25 +0100

    documentation

--- graph.c
+++ graph.c
@@ -1,13 +1,17 @@
 #include "graph.h"
 
+#include <stdio.h>
+
 struct _st_graph {};
 
 Graph *mk_graph() { return 0; }
 
 void del_graph(const Graph *graph) {}
 
+Graph *load_graph(FILE file);
+
 void add_point(Graph *graph, int x, int y) {}
 
-char *graph2string(const Graph *graph) { return 0; }
+char *graph2mmd(const Graph *graph) { return 0; }
 
 short is_in_graph(const Graph *graph, int x, int y) { return 0; }
--- graph.h
+++ graph.h
@@ -1,13 +1,66 @@
 #ifndef WOLF_GRAPH__H
 #define WOLF_GRAPH__H
 
+#include <stdio.h>
+
+/**
+ * Graph structure, hidden behind that inner API.
+ * @author Khelda
+ */
 typedef struct _st_graph Graph;
 
+/**
+ * Empty graph builder. It may be useful to call this
+ * before having to sort anything in.
+ * @return a graph which knows no nodes.
+ */
 Graph *mk_graph();
+
+/**
+ * Graph deletion from memory.
+ * @param graph the pointer towards the graph.
+ * @pre @p graph must be not null.
+ */
 void del_graph(const Graph *graph);
 
+/**
+ * Graph loader from a file. The graph should know the
+ * points listed in the file.
+ * @param file the file descriptor we want to read from.
+ * @return a new graph made from the file informations.
+ * @rep @p file must point to an open JSON file
+ */
+Graph *load_graph(FILE file);
+
+/**
+ * Let the graph know of a "new" point. In case of already
+ * known points, it will silentlty fail.
+ * @param graph the graph we're writing on.
+ * @param x the x-coordinate. It will not be checked.
+ * @param y the y-coordinate. It will not be checked.
+ * @warning in case of aberrant coordinates, the graph
+ * may turn out... Weird.
+ * @pre @p graph must be a valid pointer.
+ */
 void add_point(Graph *graph, int x, int y);
-char *graph2string(const Graph *graph);
+
+/**
+ * Turning our graph into a mermaid string
+ * @param graph the graph to transform.
+ * @return a mermaid file content ready for output.
+ * @pre @p graph must be a valid pointer.
+ */
+char *graph2mmd(const Graph *graph);
+
+/**
+ * @param graph
+ * @param x
+ * @param y
+ * @return
+ * @pre @p graph must be a valid pointer.
+ */
 short is_in_graph(const Graph *graph, int x, int y);
 
 #endif // WOLF_GRAPH__H
+
+// vim: ft=c.doxygen
