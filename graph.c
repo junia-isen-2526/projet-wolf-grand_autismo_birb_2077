a2f43c44115835e03f2c283c198d7a3cbb6fdc91
kheldae <taho.gougeon@gmail.com>
2025-12-09 10:49:06 +0100

    add vertices

--- graph.c
+++ graph.c
@@ -3,11 +3,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct {
+typedef struct st_point Point;
+
+struct st_point {
   unsigned int name;
   unsigned int x;
   unsigned int y;
-} Point;
+  // link list of points
+  Point *next;
+};
 
 typedef struct st_edge Edge;
 
@@ -22,29 +26,36 @@ struct _st_graph {
   unsigned int nb_vertices;
   Edge *edges;
   // keeping current point in mind
-  Point *point;
+  Point *points;
 };
 
 Graph *mk_graph() {
   Graph *graph = malloc(sizeof(Graph));
   graph->nb_vertices = 0;
-  graph->edges = 0; // no known edges for now
-  graph->point = 0;
+  graph->edges = 0;  // no known edges for now
+  graph->points = 0; // no known points for now
   return graph;
 }
 
-void del_edge(Edge *e) {
+void del_edges(Edge *e) {
   if (!e)
     return;
-  del_edge(e->next);
+  del_edges(e->next);
   free(e);
+}
+
+void del_points(Point *p) {
+  if (!p)
+    return;
+  del_points(p);
+  free(p);
 }
 
 void del_graph(Graph *graph) {
   if (!graph)
     return; // silent fail in case of no pointer
-  del_edge(graph->edges);
-  free(graph->point);
+  del_edges(graph->edges);
+  del_points(graph->points);
   free(graph);
 }
 
@@ -54,17 +65,50 @@ Graph *load_graph(FILE *file) {
   return 0;
 }
 
+// unique id generator
+unsigned int get_id() {
+  static long nextId = 0;
+  return ++nextId;
+}
+
+// inner add method for graphs
+void add_vertex(Graph *graph, unsigned int id, unsigned int x, unsigned int y) {
+  if (!graph)
+    return;
+  // seeking an existing point
+  Point *p = graph->points;
+  for (p = graph->points; p; p = p->next)
+    if (p->name == id)
+      return; // the point already exist
+  Point *pp = malloc(sizeof(Point));
+  pp->name = id;
+  pp->x = x;
+  pp->y = y;
+  if (!p) // no points yet
+    p = pp;
+  else
+    p->next = pp;
+}
+
 void add_point(Graph *graph, unsigned int x, unsigned int y) {
   if (!graph)
     return; // silent fail
+  add_vertex(graph, get_id(), x, y);
 }
 
 void graph2mmd(const Graph *graph, FILE *file) {
   if (!graph || !file)
     return; // not my problem then
-  fprintf(file, "TD");
+  short known[graph->nb_vertices];
+  for (int i = 0; i < graph->nb_vertices; i++)
+    known[i] = 0;
+  // actual writing
+  fprintf(file, "graph TD");
   for (Edge *e = graph->edges; e; e = e->next) {
-    fprintf(file, ""); // TODO find what to print
+    fputc('\t', file);
+    // TODO
+    fprintf(file, " -> ");
+    fputc('\n', file);
   }
 }
 
