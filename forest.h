798945f722fe538fdf3a1d429e7e184b31e4946a
kheldae <taho.gougeon@gmail.com>
2026-01-20 17:25:35 +0100

    making foresty type

---
+++ forest.h
@@ -0,0 +1,14 @@
+#ifndef __FOREST__H
+#define __FOREST__H
+
+#include "wolf.h"
+
+/**
+ * Custom type to encapsulate a Forest's behavior.
+ * @author kheldae
+ */
+typedef char Forest[FOREST_HEIGHT][FOREST_WIDTH];
+
+#endif // __FOREST__H
+
+// vim: ft=c.doxygen
